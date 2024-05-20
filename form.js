ba14919614df2fa8f5e9a0e64bad4f9066559a6b
unknown <ifficodder@gmail.com>
2024-05-21 03:21:01 +0500

    login-form

---
+++ form.js
@@ -0,0 +1,15 @@
+const username = document.querySelector('#username');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const form = document.querySelector('#form');
+
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault()
+    console.log(username.value);
+    console.log(email.value);
+    console.log(password.value);
+})
+
+
+
